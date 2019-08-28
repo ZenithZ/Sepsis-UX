934da37ab03fae30cc3f6e6592b8229b1edea533
ppor2353 <ppor2353@uni.sydney.edu.au>
2019-08-28 20:02:04 +1000

    Added basic table for #3, however formatted style will likely change

--- dashboard/src/app/components/detail/detail.component.ts
+++ dashboard/src/app/components/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { Patient } from '../table/table.component';
+import { MatTableDataSource, MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-detail',
@@ -10,6 +11,10 @@ export class DetailComponent implements OnInit {
 
   @Input() patient: Patient;
 
+  displayedColumns: string[] = ['Stat', 'Value', 'Time'];
+  data: string[] = ['BT', 'PR', 'RR', 'BP'];
+  dataSource;
+
   val:number = 15;
   selected:string = "Yes";
   loC: string = "Alert";
@@ -17,11 +22,19 @@ export class DetailComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let patientDataArray = [
+      {'stat': 'Body Temperature', 'value': this.patient.BT, 'time': this.patient.BT_time},
+      {'stat': 'Pulse Rate', 'value': this.patient.PR, 'time': this.patient.PR_time},
+      {'stat': 'Respiration Rate', 'value': this.patient.RR, 'time': this.patient.RR_time},
+      {'stat': 'Blood Pressure', 'value': this.patient.BP, 'time': this.patient.BP_time}
+    ];
+    this.dataSource = new MatTableDataSource(patientDataArray);
   }
 
   formatLabel(value:number) {
     return DetailComponent.getLabel(value);
   }
+
 
   setLoCText() {
     this.loC = DetailComponent.getLabel(this.val);
