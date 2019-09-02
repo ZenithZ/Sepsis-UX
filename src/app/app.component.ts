2f0fe3af6d5740ea85cb624215c887aed50bc46a
John R. Spicer <johnrspicer@hotmail.com>
2019-09-03 00:26:19 +1000

    app component now manages data organ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import sampleData from '../../REST-data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dashboard';
+export class AppComponent implements OnInit {
+  ats = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: []
+  }
+
+  data = sampleData.slice(1, 10);
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    var dLen = this.data.length;
+    for (var i = 0; i < dLen; i++) {
+      var r = Math.floor((Math.random() * 5) + 1);
+      this.ats[r].push(this.data[i]);
+    }
+    console.log(this.ats);
+  }
 }
