de580b51708c6330a335449a83ebbdd940a8f3ef
ppor2353 <ppor2353@uni.sydney.edu.au>
2019-11-02 16:20:14 +1100

    Updated serve.sh to ensure that no other instance is running beforehand

--- testing/serve.sh
+++ testing/serve.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+pid=$(ps -x | grep 'ng serve' | head -1 | cut -d' ' -f1)
+if [ ! -z "$pid" ]
+then
+	kill $pid
+fi
 cd ../dashboard
 $(ng config -g cli.warnings.versionMismatch false)
 $(ng serve > /dev/null)
