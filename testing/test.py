854419d3e6fcb622ba59b17389a5c43a24b9b13b
ppor2353 <ppor2353@uni.sydney.edu.au>
2019-11-02 22:08:02 +1100

    Removed unused tests (waittime and view toggle)

--- testing/test.py
+++ testing/test.py
@@ -134,49 +134,6 @@ def test_page_load():
     res = len(errors) == 0
     return res, 'Console errors present' if not res else None
 
-
-def test_view_toggle():
-    global DRIVER
-    
-    button = DRIVER.find_element_by_xpath("/html/body/app-root/div/mat-form-field/div/div[1]/div[2]/mat-icon")
-
-    # Determine original configuration
-    orig_all_tables = DRIVER.find_elements_by_css_selector("app-table")
-    orig_unseen_tables = DRIVER.find_elements_by_xpath("/html/body/div[2]/div[2]/div/div")
-    if not button or not orig_all_tables or not orig_unseen_tables:
-        return FAIL, 'Button, or table not present'
-    
-    n_tables = len(orig_all_tables)
-    orig_diff = n_tables - len(orig_unseen_tables)
-    new_diff = len(orig_all_tables) - orig_diff
-    if orig_diff != 1 and orig_diff != 4:
-        return FAIL, 'Incorrect number of tables'
-
-    button.click()
-    
-    # After clicking the button the number of tables seen tables should change
-    new_all_tables = DRIVER.find_elements_by_css_selector("app-table")
-    new_unseen_tables = DRIVER.find_elements_by_xpath("//app-table[contains(@class, 'unseen')]")
-    if not new_all_tables or not new_unseen_tables or not len(new_all_tables) == n_tables:
-        return FAIL, 'At least one table that should be present is not present'
-    change = len(new_all_tables) - len(new_unseen_tables)
-    if not change == new_diff:
-        return FAIL, 'Incorrect number of tables'
-
-    button.click()
-
-    # Ensure that reclicking the button returns to the original state
-    final_all_tables = DRIVER.find_elements_by_css_selector("app-table")
-    final_unseen_tables = DRIVER.find_elements_by_xpath("//app-table[contains(@class, 'unseen')]")
-    if not final_all_tables or not new_unseen_tables or not len(final_all_tables) == n_tables:
-        return FAIL, 'At least one table that should be present is not present'
-    change = len(final_all_tables) - len(final_unseen_tables)
-    if not change == orig_diff:
-        return FAIL, 'Incorrect number of tables'
-    
-    return PASS, None
-
-
 def test_name_search():
     global DRIVER
 
@@ -234,39 +191,6 @@ def test_MRN_search():
     mrns = DRIVER.find_elements_by_xpath("//td[contains(@class, 'MRN')]")
     if len(mrns) > 0:
         return FAIL, 'Search did not produce correct results'
-
-    return PASS, None
-
-
-def test_sort_waittime():
-    global DRIVER
-
-    button = DRIVER.find_element_by_xpath("/html/body/app-root/div/app-table[5]/div/table/thead/tr/th[9]/div/button")
-    button.click()
-
-    tables = DRIVER.find_elements_by_tag_name("app-table")
-    tables = [t for t in tables if "unseen" not in t.get_attribute('class')]
-
-    patients = []
-    for t in tables:
-        patients += t.find_elements_by_class_name("example-element-row")
-
-    wait = 100 * 24 * 60
-    prev = "100d 00:00"
-    for p in patients:
-        time = ' '.join(p.find_element_by_class_name("cdk-column-Delta").text.split()[:2])
-        days = time.split("d")
-        hours_min = days[1].split(":")
-
-        days = int(days[0]) * 24 * 60
-        hours = int(hours_min[0]) * 60
-        minutes = int(hours_min[1][1])
-        current_wait = days + hours + minutes
-        if current_wait > wait:
-            print(prev, time, current_wait, wait)
-            return FAIL, 'Waiting time does not get correctly sorted'
-        wait = current_wait
-        prev = time
 
     return PASS, None
 
@@ -809,10 +733,8 @@ def get_testcases():
     if not SKIP:
         tests.append(Test('Build', test_build))
     tests.append(Test('Test Page Load', test_page_load))
-    # tests.append(Test('Test Tables Toggle', test_view_toggle)
     tests.append(Test('Test Name Search', test_name_search))
     tests.append(Test('Test MRN Search', test_MRN_search))
-    # tests.append(Test('Test Waiting Time Sorting', test_sort_waittime))
 
     tests.append(Test('Item 3 - Test 6: Repeated Vitals in order', test_repeated_vitals))
     tests.append(Test('Item 3 - Test 7: Repeated Bloodgas in order', test_repeated_bloodgas))
