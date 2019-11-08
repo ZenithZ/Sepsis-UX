52bf94ffa3ac19470dfeb7812e2b84cdb30f70b3
ppor2353 <ppor2353@uni.sydney.edu.au>
2019-11-08 13:27:48 +1100

    New patients start waiting time at 00:00

--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -21,43 +21,56 @@ export class AppComponent implements OnInit {
     let dLen = this.data.length;
     for (let i = 0; i < dLen; i++) {
       let p = this.data[i]
-      this.addPatient(p)
+      this.addPatient(p, false)
     }
 
     setTimeout(() => {
-      this.addPatient(sampleData[8]);
+      this.addPatient(sampleData[8], true);
     }, 10000);
     setTimeout(() => {
-      this.addPatient(sampleData[9]);
+      this.addPatient(sampleData[9], true);
     }, 15000);
     setTimeout(() => {
-      this.addPatient(sampleData[10]);
+      this.addPatient(sampleData[10], true);
     }, 17000);
     setTimeout(() => {
-      this.addPatient(sampleData[11]);
+      this.addPatient(sampleData[11], true);
     }, 23000);
     setTimeout(() => {
-      this.addPatient(sampleData[12]);
+      this.addPatient(sampleData[12], true);
     }, 30000);
     setTimeout(() => {
-      this.addPatient(sampleData[13]);
+      this.addPatient(sampleData[13], true);
     }, 35000);
   }
 
-  addPatient(patient: any) {
+  addPatient(patient: any, newPatient: boolean) {
     // console.log(patient)
     let now = new Date();
     let year = now.getFullYear();
     let month = ('00' + (now.getMonth() + 1)).slice(-2);
     let day = ('00' + now.getDate()).slice(-2);
-    let hour = ('00' + (now.getHours() - Math.floor(Math.random() * now.getHours() + 1))).slice(-2);
-    if (Number(hour) < 0) {
-      hour = String(24 + Number(hour));
-      day = String(Number(day) - 1);
+    let min = ('00' + (now.getMinutes() - 1 % 60)).slice(-2);
+    let hour = ('00' + now.getHours()).slice(-2);
+    if (Number(min) > now.getMinutes()) {
+      hour = String(Number(hour) - 1);
     }
-    let min = ('00' + (now.getMinutes() + Math.floor(Math.random() * 60)) % 60).slice(-2);
-    let sec = ('00' + now.getSeconds()).slice(-2);
-    let reg = year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' +  sec;
+    if (Number(hour) < 0) {
+      hour = '23';
+    }
+    let sec = ('00' + ((now.getSeconds()) % 60)).slice(-2);
+    let reg = year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec;
+    console.log(reg);
+    if (!newPatient) {
+      hour = ('00' + (now.getHours() - Math.floor(Math.random() * now.getHours() + 1))).slice(-2);
+      if (Number(hour) < 0) {
+        hour = String(24 + Number(hour));
+        day = String(Number(day) - 1);
+      }
+      min = ('00' + (now.getMinutes() + Math.floor(Math.random() * 60)) % 60).slice(-2);
+      sec = ('00' + now.getSeconds()).slice(-2);
+      reg = year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' +  sec;
+    }
     patient['Registration'] = reg;
     this.combinedats.push(patient);
     this.combinedats = [...this.combinedats];
